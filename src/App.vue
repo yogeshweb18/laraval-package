72a78bb79c7e715020ed59923b4fc47d4d44a6cd
unknown <yogesh.thakur@acc.ltd>
2023-09-02 21:17:07 +0530

    third

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="nav">
-      <router-link to="/home">Home</router-link> |
-      <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
-</template>
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-//import App from './pages/Tool.vue'
-import router from './router' // <---
-
-createApp(App).use(router).mount('#app')
